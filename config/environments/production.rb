2d30d4bae7fb23461d325d219c1b4bd016e35580
Anita <amundurraga@uc.cl>
2021-04-10 09:49:44 -0400

    hola2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,8 +2,11 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
+  #
   config.cache_classes = true
-
+  config.cache_classes = true
+  
+  config.assets.digest = true
   config.serve_static_assets = true
   config.assets.compile = true
   # Eager load code on boot. This eager loads most of Rails and
